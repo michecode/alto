31c1a4d45f86aa41c533193339e5ef435b7c3523
maddy <maddy.fries@protonmail.com>
2021-06-09 17:24:03 -0700

    mayhaps will work on render :)

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,59 +1,101 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
 import PlantPic from './PlantPic';
 import 'fontsource-shrikhand/400-normal.css';
 
 const Card = styled(Paper)`
-    // height: 10vh;
-    && {
-        background-color: var(--color-paper);
-        border-radius: 10px;
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-    }
-`
+  // height: 10vh;
+  && {
+    background-color: var(--color-paper);
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+`;
 
 const Wrapper = styled.div`
-    margin: 5px auto;
-    // height: 75%;
-`
+  margin: 5px auto;
+  // height: 75%;
+`;
 
 const Title = styled.p`
-    font-family: shrikhand;
-    font-size: 20px;
-    // font-size: 100%;
-    text-align: center;
-    color: var(--color-text);
-    margin: 10px 0px 10px 0px;
-`
+  font-family: shrikhand;
+  font-size: 20px;
+  // font-size: 100%;
+  text-align: center;
+  color: var(--color-text);
+  margin: 10px 0px 10px 0px;
+`;
 /*
 Basically I'm defining the picture size at first paint. If they resize they can refresh the page
 idgaf. 
 */
 export default function Plantcard(props) {
-    // default height and width is 150px.
-    let y=150, x=150;
-    /*
-    When screen width is below 600px keep at 150px.
-    */
-    if(window.screen.availWidth <= 600);
-    else if(window.screen.availWidth > 600 && window.screen.availWidth < 1275) {
-        y = 200;
-        x = 200;
-    }
-    else if(window.screen.availWidth >= 1275) {
-        y=250;
-        x=250;
-    }
+  // default height and width is 150px.
+  let y = 150,
+    x = 150;
 
-    return (
-        <Card>
-            <Wrapper>
-                <PlantPic height={y} width={x} filtering={true} id={props.id} name={props.name}/>
-            </Wrapper>
-            <Title>{props.name}</Title>
-        </Card>
-    )
+  /* 
+  useEffect with the [] is basically componentDidMount for some reason.
+  this is so that window can run on the server
+  MAY CAUSE BUGS IN THE FUTURE!!
+  */
+  //   useEffect(() => {
+  //       console.log('yello');
+  //     if (window.screen.availWidth <= 600);
+  //     else if (
+  //       window.screen.availWidth > 600 &&
+  //       window.screen.availWidth < 1275
+  //     ) {
+  //         console.log('Screen Width: ' + window.screen.availWidth + '. Setting /plants pictures to 200x200');
+  //       y = 200;
+  //       x = 200;
+  //     } else if (window.screen.availWidth >= 1275) {
+  //         console.log('Screen Width: ' + window.screen.availWidth + '. Setting /plants pictures to 200x200');
+  //       y = 250;
+  //       x = 250;
+  //     }
+  //   }, []);
+  if (typeof window !== 'undefined') {
+    if (window.screen.availWidth <= 600);
+    else if (
+      window.screen.availWidth > 600 &&
+      window.screen.availWidth < 1275
+    ) {
+      console.log(
+        'Screen Width: ' +
+          window.screen.availWidth +
+          '. Setting /plants pictures to 200x200'
+      );
+      y = 200;
+      x = 200;
+    } else if (window.screen.availWidth >= 1275) {
+      console.log(
+        'Screen Width: ' +
+          window.screen.availWidth +
+          '. Setting /plants pictures to 200x200'
+      );
+      y = 250;
+      x = 250;
+    }
+  }
+
+  console.log(y);
+
+  return (
+    <Card>
+      <Wrapper>
+        <PlantPic
+          height={y}
+          width={x}
+          filtering={true}
+          id={props.id}
+          name={props.name}
+        />
+      </Wrapper>
+      <Title>{props.name}</Title>
+    </Card>
+  );
 }
--- src/pages/succulents/index.js
+++ src/pages/succulents/index.js
@@ -10,7 +10,7 @@ export default function Index(props) {
 
     return (
         <>
-            
+            hello
         </>
     )
 }
