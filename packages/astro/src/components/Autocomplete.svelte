7d7b6a8af42d3ddbc05f11e4833bbb97211326ee
michecode <mfrs@protonmail.com>
2022-12-08 20:34:36 -0800

    mobile search view

--- packages/astro/src/components/Autocomplete.svelte
+++ packages/astro/src/components/Autocomplete.svelte
@@ -1,29 +1,31 @@
 <script lang="ts">
-  // At build I need to query for the list of names
+  import Icon from './Icon.svelte';
+  import colors from '../styles/colors';
+
   export let plants: SearchCollection = [];
 
   let filteredPlants: SearchObject[] = [];
-
-  const filterPlants = () => {
-    filteredPlants = plants.filter((plant) => {
-      return plant.name.toLowerCase().startsWith(inputValue.toLowerCase());
-    });
-    filteredPlants = filteredPlants.slice(0, 5);
-  };
-
   let inputValue: string = '';
+  let hiLiteIndex: number = -1;
+  let mobileSearchScreen = true;
 
   $: if (!inputValue) {
     filteredPlants = [];
     hiLiteIndex = -1;
   }
+  $: hiLitedPlant = filteredPlants[hiLiteIndex] ?? { name: '', slug: '' };
+
+  const filterPlants = (_: Event | null, endIndex: number = 5) => {
+    if (!inputValue) return;
+    filteredPlants = plants.filter((plant) => {
+      return plant.name.toLowerCase().startsWith(inputValue.toLowerCase());
+    });
+    filteredPlants = filteredPlants.slice(0, endIndex);
+  };
 
   const navigate = (target: string) => {
     window.location.href = `/plants/${target}`;
   };
-
-  let hiLiteIndex: number = -1;
-  $: hiLitedPlant = filteredPlants[hiLiteIndex] ?? { name: '', slug: '' };
 
   const navigateList = (e: KeyboardEvent) => {
     const max = filteredPlants.length - 1;
@@ -39,6 +41,20 @@
       return;
     }
   };
+
+  // Opens the mobile full screen search
+  const handleClick = () => {
+    if (window.innerWidth < 1024) {
+      mobileSearchScreen = true;
+    }
+  };
+
+  const handleClose = () => {
+    mobileSearchScreen = false;
+    // If user closes menu with text in input clear it.
+    inputValue = '';
+    filterPlants(null, 5);
+  };
 </script>
 
 <div
@@ -52,6 +68,7 @@
       bind:value={inputValue}
       on:input={filterPlants}
       on:keydown={navigateList}
+      on:click={handleClick}
     />
   </div>
   <!-- FILTERED LIST -->
@@ -73,3 +90,47 @@
     </ul>
   {/if}
 </div>
+<!-- MOBILE SEARCH! -->
+{#if mobileSearchScreen}
+  <div
+    class="fixed top-0 left-0 px-4 w-full h-full z-20 bg-white flex flex-col"
+  >
+    <div class="flex items-center">
+      <input
+        type="text"
+        placeholder="Search for Plants"
+        class="w-full p-4 rounded-2xl focus:outline-none"
+        bind:value={inputValue}
+        on:input={() => filterPlants(null, 10)}
+        on:keydown={navigateList}
+        on:click={handleClick}
+      />
+      <button on:click={handleClose}>
+        <Icon
+          type="close"
+          focusable
+          fill={colors.black}
+          height="32px"
+          width="32px"
+        />
+      </button>
+    </div>
+    <hr />
+    {#if filteredPlants.length > 0}
+      <ul class="mx-4 my-4">
+        {#each filteredPlants as { name, slug }, index}
+          <li
+            class={`py-1 rounded-lg ${
+              hiLitedPlant.name === name ? 'bg-black text-white' : ''
+            }`}
+            on:mouseenter={() => (hiLiteIndex = index)}
+            on:click={() => navigate(slug)}
+            on:keydown={navigateList}
+          >
+            <p class="text-lg">{name}</p>
+          </li>
+        {/each}
+      </ul>
+    {/if}
+  </div>
+{/if}
