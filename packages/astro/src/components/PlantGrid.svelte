9a3043040b5dc911170eb0005bf5785a6695aee2
michecode <mfrs@protonmail.com>
2022-12-14 20:22:33 -0800

    clean up

--- packages/astro/src/components/PlantGrid.svelte
+++ packages/astro/src/components/PlantGrid.svelte
@@ -6,19 +6,13 @@
 
   $: filterPlants($filters);
 
-  // Hi future maddy. This function is a bit messy because I check if all the checkbox filters are the same
-  // because if theyre all disabled then you should display all yknow. Ok sorry i didnt wanna make an
-  // isAllSame function because i tried it and it literally blows job so kys.
-  // NVMMMM IM A HYPER JEANYIS LMFOA
   // ts ignoring because filters and plant are already tightly controlled
   // @ts-ignore
   const filterPlants = (filters: Filters) => {
     filteredPlants = plants.filter((plant) => {
-      console.log(plant);
       // Classification
       // If class isn't selected && theyre not all enabled/disabled then filter
       if (!filters.classification[plant.class_peek.toLowerCase()]) {
-        console.log('i dont match!', plant.name);
         const pairs = Object.entries(filters.classification);
         if (checkPairs(pairs)) {
           return false;
