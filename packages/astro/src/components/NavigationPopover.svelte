72129d24178737fd6c3ddb118ef7ba84a8ed4a20
michecode <mfrs@protonmail.com>
2023-01-23 19:09:24 -0800

    remove old popper components

--- packages/astro/src/components/NavigationPopover.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import Popper from "./Popper.svelte";
-</script>
-
-<Popper on:show tooltip={false} placement={"bottom-start"} triggeredBy={$$props.triggeredBy}>
-  <slot/>
-</Popper>
