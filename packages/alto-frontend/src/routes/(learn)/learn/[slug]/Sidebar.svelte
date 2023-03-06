ad60ef9eee32518f8578dd9aa84c75a034cd537b
michecode <mfrs@protonmail.com>
2023-03-05 17:49:18 -0800

    broken sidebar in learn

--- packages/alto-frontend/src/routes/(learn)/learn/[slug]/Sidebar.svelte
+++ packages/alto-frontend/src/routes/(learn)/learn/[slug]/Sidebar.svelte
@@ -2,13 +2,15 @@
 	export let articles: Article[];
 </script>
 
-<div class="m-2">
-	{#each articles as article}
-		<a href={article.category.toLowerCase()}>
-			<h6 class="font-serif font-semibold text-xl">{article.category}</h6>
-		</a>
-		{#each article.sections as section}
-			<p>{section.header}</p>
+<div class="hidden lg:block lg:min-w-[20%] border-r-2 border-byz dark:border-white">
+	<div class="m-2">
+		{#each articles as article}
+			<a href={article.category.toLowerCase()}>
+				<h6 class="font-serif font-semibold text-xl">{article.category}</h6>
+			</a>
+			{#each article.sections as section}
+				<p>{section.header}</p>
+			{/each}
 		{/each}
-	{/each}
+	</div>
 </div>
